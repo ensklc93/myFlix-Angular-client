b3d943afc8692be4a6c589900610482060c59ea4
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-09-24 19:02:18 +0200

    Added component movie-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 
 
@@ -13,15 +14,20 @@ export class AppComponent {
   title = 'myFlix-Angular-client';
 
   constructor(public dialog: MatDialog) { }
-// Function that will open the dialog when the signup button is clicked  
-openUserRegistrationDialog(): void {
+  // Function that will open the dialog when the signup button is clicked  
+  openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
-    width: '280px'
+      width: '280px'
     });
   }
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px'
-      });
-    }
+    });
+  }
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
+  }
 }
