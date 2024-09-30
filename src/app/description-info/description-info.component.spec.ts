6b5c0d5660696037b88a0bb7fb1257693134a9e0
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-09-30 17:32:28 +0200

    Added Description component for movies to show detailed desciption

---
+++ src/app/description-info/description-info.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DescriptionInfoComponent } from './description-info.component';
+
+describe('DescriptionInfoComponent', () => {
+  let component: DescriptionInfoComponent;
+  let fixture: ComponentFixture<DescriptionInfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DescriptionInfoComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DescriptionInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
